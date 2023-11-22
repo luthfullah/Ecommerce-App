f91645392cfc798fa2ff0622542892ae9c9ab34e
luthfullah <luthfullah111@gmail.com>
2023-11-23 00:35:24 +0500

    fifth commit

--- database/db.js
+++ database/db.js
@@ -1,6 +1,13 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('ecommerce', 'postgres', '123', {
-  host: 'localhost',
-  dialect: 'postgres'
+
+const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:123@localhost:5432/ecommerce', {
+  dialect: 'postgres',
+  logging: false, // Set to true to log SQL queries, if needed
+  define: {
+    timestamps: false, // Disable timestamps (createdAt and updatedAt) if not needed
+  },
+ 
 });
+
 module.exports = sequelize;
+
